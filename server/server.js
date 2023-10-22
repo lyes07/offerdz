89e810601d483b2769379f0ab20442710fd08fcb
Lyes Grine <83643064+lyes07@users.noreply.github.com>
2023-10-22 11:44:37 +0100

    adding routes to the server

--- server/server.js
+++ server/server.js
@@ -5,10 +5,40 @@ const cors = require('cors')
 const session = require('express-session')
 const bcrypt = require('bcryptjs')
 
+const userRoutes = require('./routes/Users')
+const postRoutes = require('./routes/Posts')
+
 const server = express()
+const port = process.env.PORT || process.env.SECONDPORT
+
+server.use(express.urlencoded({extended : true}))
+server.use(express.json())
+server.use(cors({
+    origin : `http://localhost:${process.env.FRONTENDPORT}`,
+    methods : ["POST","GET","DELETE","PUT","HEAD","OPTIONS"],
+    credentials : true
+}))
+
+const store = new (require('connect-pg-simple')(session))()
+server.use(
+    session(
+        {
+           store : store,
+           secret : process.env.SESSIONSECRET,
+           resave : false,
+           saveUninitialized : false,
+           cookie : {
+                sameSite : false,
+                secure : false,
+                httpOnly : false,
+                maxAge : 1000 * 60 * 60 * 24
+           } 
+        }
+    )
+)
+
+server.use('/user', userRoutes)
+server.use('/post', postRoutes)
 
 
-const port = process.env.PORT
-
-server.get('/', (req, res) => res.json({text :'Heo orld!'}))
 server.listen(port, () => console.log(`Server is listening on port ${port}!`))
